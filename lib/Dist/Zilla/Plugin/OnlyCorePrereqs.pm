29801eef474c18a09dc2b4f3e6fbaf514bed3af6
Karen Etheridge <ether@cpan.org>
2015-03-13 22:11:00 -0700

    increment $VERSION after release

--- lib/Dist/Zilla/Plugin/OnlyCorePrereqs.pm
+++ lib/Dist/Zilla/Plugin/OnlyCorePrereqs.pm
@@ -5,7 +5,7 @@ package Dist::Zilla::Plugin::OnlyCorePrereqs;
 # KEYWORDS: plugin distribution metadata prerequisites core
 # vim: set ts=8 sts=4 sw=4 tw=78 et :
 
-our $VERSION = '0.023';
+our $VERSION = '0.024';
 
 use Moose;
 with 'Dist::Zilla::Role::AfterBuild';
